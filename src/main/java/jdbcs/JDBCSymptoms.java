a4b13f7e8bf064735c217a260b156dc378c49490
pablorosalesrodriguez <pablo.rosalesrodriguez@usp.ceu.es>
2024-11-14 17:36:51 +0100

    symptoms y su jdbc en server

---
+++ src/main/java/jdbcs/JDBCSymptoms.java
@@ -0,0 +1,37 @@
+package jdbcs;
+
+import POJOS.Symptoms;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class JDBCSymptoms {
+    private JDBCManager manager;
+
+    public JDBCSymptoms(JDBCManager manager) {
+        this.manager = manager;
+    }
+
+    public ArrayList<Symptoms> readSymptoms() {// read table Patients from db
+        ArrayList<Symptoms> symptoms = new ArrayList<>();
+        String sql = "SELECT * FROM Symptoms;";
+
+        try {
+            Statement stmt = manager.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                Symptoms symptom = new Symptoms(id, name);
+
+                symptoms.add(symptom);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return symptoms;
+    }
+}
