94ba5d2ef2af834df5fe9e34007476c613aa2b78
phita <pablo.hitalara@usp.ceu.es>
2024-11-17 11:39:51 +0100

    patient interface done

--- src/main/java/ifaces/PatientManager.java
+++ src/main/java/ifaces/PatientManager.java
@@ -1,4 +1,16 @@
 package ifaces;
 
+import Pojos.Patient;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public interface PatientManager {
+    public ArrayList<Patient> readPatients();
+    public void addPatient(String name, String surname, LocalDate dob, String email);
+    public int getId(String name);
+    public Integer emailToId(String email);
+    public Patient getPatientFromUser(int user_id);
+    public Patient getPatientFromId(Integer id);
+    public Patient getPatientFromEmail(String email);
 }
